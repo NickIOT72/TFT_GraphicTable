7a0ae8cda3394d7519b1b9d7458e5b1642a24232
NickIOT72 <niko722795@gmail.com>
2024-03-17 13:54:46 -0400

    On this branch: * Were added and evaluated the libraries to test the Arduino UNO with the TFT ILI9486
    Signed-off-by: NickIOT72 <niko722795@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,63 @@
-#include <Arduino.h>
+#include <Adafruit_GFX.h> // Hardware-specific library
+#include <MCUFRIEND_kbv.h>
+MCUFRIEND_kbv tft;
 
-// put function declarations here:
-int myFunction(int, int);
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define RGB(r, g, b) (((r&0xF8)<<8)|((g&0xFC)<<3)|(b>>3))
+
+#define GREY      RGB(127, 127, 127)
+#define DARKGREY  RGB(64, 64, 64)
+#define TURQUOISE RGB(0, 128, 128)
+#define PINK      RGB(255, 128, 192)
+#define OLIVE     RGB(128, 128, 0)
+#define PURPLE    RGB(128, 0, 128)
+#define AZURE     RGB(0, 128, 255)
+#define ORANGE    RGB(255,128
+
+#define LANDSCAPE 1
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    tft.reset();
+    uint16_t identifier = tft.readID();
+    Serial.print("ID = 0x");
+    Serial.println(identifier, HEX);
+    if (identifier == 0xEFEF) identifier = 0x9486;
+    tft.begin(identifier);
+    tft.fillScreen(WHITE);
+     tft.setRotation(LANDSCAPE);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+char *msg[] = { "PORTRAIT", "LANDSCAPE", "PORTRAIT_REV", "LANDSCAPE_REV" };
+uint8_t aspect;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    uint16_t x = 50, y = 100;
+    tft.setRotation(aspect);
+    tft.fillScreen(0x0000);
+    tft.setCursor(0, 0);
+    tft.setTextSize(2);
+    tft.println(msg[aspect]);
+    tft.setCursor(x, y);
+    tft.println("[x=" + String(x) + ",y=" + String(y) + "]");
+    delay(5000);
+    tft.println("INVERT ON");
+    tft.invertDisplay(true);
+    delay(1000);
+    tft.invertDisplay(false);
+    tft.println("INVERT OFF");
+    delay(1000);
+    if (++aspect >= 4) aspect = 0;
 }
